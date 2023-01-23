3dc2dc41376c0aaeb0ad6a7f34f531cfea845670
Chris <123cmorales@frhsd.com>
2023-01-23 08:08:21 -0500

    Update

--- is219-p2/js/gallery.js
+++ is219-p2/js/gallery.js
@@ -33,11 +33,12 @@ function animate() {
 /************* DO NOT TOUCH CODE ABOVE THIS LINE ***************/
 
 function swapPhoto() {
+	document.getElementById('photo').src = mImages[0];
+	console.log('swap photo');
 	//Add code here to access the #slideShow element.
 	//Access the img element and replace its source
 	//with a new image from your images array which is loaded 
 	//from the JSON string
-	console.log('swap photo');
 }
 
 // Counter for the mImages array
@@ -50,12 +51,20 @@ var mRequest = new XMLHttpRequest();
 var mImages = [];
 
 // Holds the retrived JSON information
-var mJson;
+var mjason;
 
 // URL for the JSON to load by default
 // Some options for you are: images.json, images.short.json; you will need to create your own extra.json later
-var mUrl = 'insert_url_here_to_image_json';
+var mUrl = "images.json";
 
+mRequest.onreadystatechange = function (){
+	if(this.ready == 4 && this.status == 200){
+		mjason = JSON.parse(mRequest.responseText);
+		iterateJSon(mJSON);
+	}
+};
+mRequest.open("GET",mUrl, true);
+mRequest.send();
 
 //You can optionally use the following function as your event callback for loading the source of Images from your json data (for HTMLImageObject).
 //@param A GalleryImage object. Use this method for an event handler for loading a gallery Image object (optional).
@@ -82,7 +91,22 @@ window.addEventListener('load', function() {
 function GalleryImage() {
 	//implement me as an object to hold the following data about an image:
 	//1. location where photo was taken
+	let location = "";
 	//2. description of photo
+	let photo = "";
 	//3. the date when the photo was taken
+	let date = "";
 	//4. either a String (src URL) or an an HTMLImageObject (bitmap of the photo. https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement)
+	let img = "";
+}
+function iterateJSon(njson)
+{
+	for(x = 0; x < mJson.images.length; x++)
+	{
+		mImages[x] = new GalleryImage();
+		mImages[x].location = mjason.images[x].imglocation;
+		mImages[x].description = mjason.images[x].imglocation;
+		mImages[x].date = mjason.images[x].imglocation;
+		mImages[x] = mjason.images[x].imglocation;
+	}
 }
